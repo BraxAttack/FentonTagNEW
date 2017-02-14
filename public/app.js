c1285ff62332fa17923bbdf90993b6cb726f7dd5
Braxton Young <youngbraxton@southern.edu>
2017-02-13 23:00:38 -0500

    geo ip working

--- public/app.js
+++ public/app.js
@@ -18,8 +18,8 @@
 
       FTCtrl.setPageRouter = function(location) {
           FTCtrl.pageRouter = location;
-      };
 
+      };
 
 
       FTCtrl.authSignInWEmailPass = function() {
@@ -59,6 +59,24 @@
       FTCtrl.products = ["Milk", "Bread", "Cheese"];
 
 
+      FTCtrl.lookupLatLng = function() {
+          var onSuccess = function(location){
+            alert(
+                "Lookup successful:\n\n"
+                + JSON.stringify(location, undefined, 4)
+            );
+          };
+
+          var onError = function(error){
+            alert(
+                "Error:\n\n"
+                + JSON.stringify(error, undefined, 4)
+            );
+          };
+
+
+          geoip2.city(onSuccess, onError);
+      }
 
 
 
